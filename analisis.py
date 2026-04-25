ee57c1131da93093ecd2795a3ee32f359859c562
Juliana <julianajj0214@gmail.com>
2026-04-25 00:01:07 -0500

    agrego promedios por evaluacion, materia y grupo

--- analisis.py
+++ analisis.py
@@ -1,3 +1,4 @@
+"""
 Módulo de análisis de datos.
 Implementa todos los análisis solicitados: desempeño, distribución, profesores y calidad.
 """
@@ -22,3 +23,32 @@ def promedio_por_estudiante(df_calificaciones, df_matriculas, df_estudiantes):
     promedio = promedio.sort_values('promedio', ascending=False)
     
     return promedio[['nombres', 'apellidos', 'promedio']]
+
+
+def promedio_por_tipo_evaluacion(df_calificaciones):
+    """Calcula promedio por tipo de evaluación (PARCIAL_1, PARCIAL_2, EXAMEN_FINAL, TRABAJO)."""
+    promedio_tipo = df_calificaciones.groupby('tipo')[['nota']].agg(['mean', 'count', 'std']).round(2)
+    promedio_tipo.columns = ['promedio', 'cantidad', 'desviacion_std']
+    return promedio_tipo
+
+
+def promedio_por_materia(df_calificaciones, df_matriculas, df_grupos, df_materias):
+    """Calcula promedio de calificaciones por materia."""
+    # Merge para obtener materia_id
+    cal_with_mat = df_calificaciones.merge(df_matriculas[['id', 'grupo_id']], left_on='matricula_id', right_on='id')
+    cal_with_mat = cal_with_mat.merge(df_grupos[['id', 'materia_id']], left_on='grupo_id', right_on='id', suffixes=('_cal', '_grup'))
+    cal_with_mat = cal_with_mat.merge(df_materias[['id', 'nombre']], left_on='materia_id', right_on='id', suffixes=('_mat', '_mat_materias'))
+    
+    promedio = cal_with_mat.groupby('nombre')[['nota']].agg(['mean', 'count']).round(2)
+    promedio.columns = ['promedio', 'cantidad_calificaciones']
+    return promedio.sort_values('promedio', ascending=False)
+
+
+def promedio_por_grupo(df_calificaciones, df_matriculas, df_grupos, df_materias):
+    """Calcula promedio de calificaciones por grupo."""
+    cal_with_grup = df_calificaciones.merge(df_matriculas[['id', 'grupo_id']], left_on='matricula_id', right_on='id')
+    cal_with_grup = cal_with_grup.merge(df_grupos[['id', 'nombre', 'semestre', 'materia_id']], left_on='grupo_id', right_on='id', suffixes=('_mat', '_grup'))
+    
+    promedio = cal_with_grup.groupby(['nombre', 'semestre'])[['nota']].agg(['mean', 'count']).round(2)
+    promedio.columns = ['promedio', 'cantidad_calificaciones']
+    return promedio.sort_values('promedio', ascending=False)
